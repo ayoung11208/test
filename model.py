14f4fd187472c1d7e8930c2d09e9059e905cab08
ayoung11208 <96093243+ayoung11208@users.noreply.github.com>
2021-12-14 11:24:11 +0900

    Add files via upload

---
+++ model.py
@@ -0,0 +1,11 @@
+class TouchScreen:
+    orderList = []
+    qtyList = []
+
+    messageOrderLineList = []
+
+    ConsumerNumber = 1001   #고객 번호
+
+class barcodeReader:
+    outlet_1_readerResult = ""
+    outlet_2_readerResult = ""
