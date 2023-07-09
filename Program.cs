1196f714278ba744f0d5e2b825629f0c131d8f85
Choi-A-Young <ayoung11208@naver.com>
2023-07-09 21:56:21 +0900

    첫 커밋

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Foo(object arg)
+    {
+        Console.WriteLine($"Foo:{arg}, {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(3000);
+    }
+
+    public static void Main()
+    {
+        ThreadPool.QueueUserWorkItem(Foo, "Hello World");
+        ThreadPool.QueueUserWorkItem(Foo); // arg에 null 전달
+        Console.ReadLine();
+    }
+}
